dc188cc7cc60319814a8203a5cf352906ab8e5ed
Leo <leonardoandrei41@gmail.com>
2026-04-28 20:11:35 -0300

    Add prelude to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,9 @@ pub use status::Status;
 pub use outcome::Outcome;
 pub use node::Node;
 pub mod nodes;
+
+pub mod prelude {
+    pub use super::{Status, Outcome, Node};
+    pub use super::{action, once, always, sequence, parallel, race, wait, interval, repeat};
+    pub use super::nodes::*;
+}
