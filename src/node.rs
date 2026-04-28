b9900f1b863226178a356c75792d02afc0d0dd13
Leo <leonardoandrei41@gmail.com>
2026-04-28 20:12:22 -0300

    General changes to the code structure

--- src/node.rs
+++ src/node.rs
@@ -6,3 +6,13 @@ pub trait Node<C> {
     fn tick(&mut self, delta: Duration, context: &mut C) -> Outcome;
     fn reset(&mut self);
 }
+
+impl<C, F> Node<C> for F
+where
+    F: FnMut(Duration, &mut C) -> Outcome,
+{
+    fn tick(&mut self, delta: Duration, context: &mut C) -> Outcome {
+        self(delta, context)
+    }
+    fn reset(&mut self) {}
+}
